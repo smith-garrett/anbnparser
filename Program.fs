f80c6d148222961a03883c2e9db9caa923746b6a
Garrett Smith <gasmith@uni-potsdam.de>
2024-07-31 10:02:33 +0200

    Add files

---
+++ Program.fs
@@ -0,0 +1,40 @@
+﻿// Simple record type for a stack. We only need to track the number of elements on the
+// stack, so this is really simple
+type Stack = {
+    depth: int
+}
+
+// Cleaning and separating the input
+let sepInput (input: string) =
+    input.ToCharArray()
+    |> Array.toList
+    |> List.filter (fun x -> x = 'a' || x = 'b')
+
+
+/// Expected input is a string of as and bs, any other characters are ignored!
+/// Returns a boolean indicating whether the string is a member of the a^n b^n language
+let parse (input: string) =
+    let ipt = sepInput input
+
+    // Inner recursive function that works its way through the input
+    let rec innerparse lst stck =
+        match lst with
+        | [] -> stck
+        | head :: rest when head = 'a' -> innerparse rest {stck with depth = stck.depth + 1}
+        | head :: rest when head = 'b' -> innerparse rest {stck with depth = stck.depth - 1}
+        // Should never get here, but needed for completeness of pattern match
+        | head :: rest -> innerparse rest stck
+    
+    let stack = innerparse ipt {depth = 0}
+
+    match stack.depth with
+    | 0 -> true
+    | _ -> false
+
+[<EntryPoint>]
+let main args =
+    let result = parse args[0]
+    match result with
+    | true -> printf "String belongs to a^n b^n\n"
+    | _ -> printf "String doesn't belong to a^n b^n\n"
+    0
